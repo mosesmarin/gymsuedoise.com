5d8cf45b7a09b1fc806b6c7cc6d5e5768b031949
Moses Marin <moses@Mosess-MacBook-Pro.local>
2017-04-14 20:56:46 -0700

    Updated script to reach reservation page
    Updated script to reach reservation page

--- gym-sued-git.py
+++ gym-sued-git.py
@@ -16,6 +16,7 @@ sys.setdefaultencoding('utf-8')
 # Define constants
 day_mardi=1
 day_jeudi=3
+day_samedi=5
 cardio_pump=46
 paris_10_chaudron=349
 base_link_reserve='https://www.gymsuedoise.com/resa/bk/?id='
@@ -29,7 +30,7 @@ credentials = { 'em': 'e-mail@gmail.com', 'pw': 'password' }
 user_search_id=999999999
 
 #Set date for the class
-offset=5
+offset=0
 my_date = date.today()+ timedelta(days=offset)
 date_value= calendar.day_name[my_date.weekday()]
 if date_value=='Monday':
@@ -38,22 +39,6 @@ elif date_value=='Wednesday':
     day_of_class=day_jeudi
 else:
     day_of_class=0
-
-
-# These are parameters used to book a class
-# Ideally these are retrieved after logon
-# And need not be hardcoded
-#
-# Link to reserve class
-# https://www.gymsuedoise.com/resa/bk/?id=337854
-#
-# Mardi 19:30
-# Tue 4/18 is id 363837
-# Tue 4/25 is id 363838
-#
-# Jeudi 19:30
-# Thu 4/20 is id 337854
-# Thu 4/27 is id 337855
 
 
 if ( date_value=='Monday' or date_value=='Wednesday' ):    
@@ -83,17 +68,36 @@ if ( date_value=='Monday' or date_value=='Wednesday' ):
                     html_course_id=class_html[1].split('"') 
                     course_id=html_course_id[0]
         link_reserve=base_link_reserve+course_id
-        print '\nUse link below to reserve\n<br>'
+        print '\nUse link below to go to course reservation\n<br>'
         print link_reserve
-        print '<br>\nUse link above to reserve\n<br>'
+        print '<br>\nUse link above to go to course reservation\n<br>'
                           
-	    #Use link to book class
+	    #Use link to go to book class page
         print '[2]---------------------------------------------------------------------------'
         r = s.get(link_reserve) 
         print r.text
+        with open("/tmp/Output2.txt", "w") as text_file:
+            text_file.write(r.text)
+        course_id=''  
+        with open("/tmp/Output2.txt") as f:
+            for line in f:
+                if 'resa/bk2' in line:
+                    #print line
+                    class_html=line.split('action="' ) 
+                    html_course_id=class_html[1].split('"') 
+                    course_id=html_course_id[0]
+        link_reserve=course_id
+        print '\nUse link below to reserve\n<br>'
+        print link_reserve
+        print '<br>\nUse link above to reserve\n<br>'
+
+	    #Reserve a place
+        print '[3]---------------------------------------------------------------------------'	
+        r = s.get(link_reserve) 
+        print r.text    
     
 	    #logout
-        print '[3]---------------------------------------------------------------------------'	
+        print '[4]---------------------------------------------------------------------------'	
         r = s.get(logout_link) 
         print r.text
 else:
