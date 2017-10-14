1685f92435ca9f60b98c0f5b168029ca9475c8cd
Moses Marin <moses@Mosess-MacBook-Pro.local>
2017-10-14 09:26:09 -0700

    Update new agenda
    update agenda

--- gym-sued-git.py
+++ gym-sued-git.py
@@ -12,29 +12,47 @@ from datetime import timedelta
 import calendar
 import time
 import boto3
+import os
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-
+    
 print('Loading function')
 
 def lambda_handler(event, context):
+    #fetch env variables
+    pw_env=os.environ['pw']
+    em_env=os.environ['em']
+    user_search_id_env=os.environ['user_search_id']
+    topic_arn_env=os.environ['topic_arn']
+    
+
+
+
 
     # Define constants
+    day_lundi=0
     day_mardi=1
+    day_mercredi=2
     day_jeudi=3
+    day_vendredi=4
     day_samedi=5
+    day_dimanche=6
+
     cardio_pump=46
+    cardio_famille=60
     paris_10_chaudron=349
+    paris_9_bergere=1
+
     base_link_reserve='https://www.gymsuedoise.com/resa/bk/?id='
     logon_link='https://www.gymsuedoise.com/lo/'
     logout_link='https://www.gymsuedoise.com/lt/'
     search_course_link='https://www.gymsuedoise.com/cours/list/'
 
     # Define user values
-    credentials = { 'em': 'e-mail', 'pw': 'password' } 
-    user_search_id=165233376
+    credentials = { 'em': em_env, 'pw': pw_env } 
+    user_search_id=user_search_id_env
     
     #Define vars for SNS message
     text_reserve=''
@@ -47,15 +65,23 @@ def lambda_handler(event, context):
     if date_value=='Monday':
         day_of_class=day_mardi
         time_of_class='19:30'
+        place_of_class=paris_10_chaudron
+        cardio_class=cardio_pump
     elif date_value=='Wednesday':
         day_of_class=day_jeudi
         time_of_class='19:30'
-    elif date_value=='Friday':
-        day_of_class=day_samedi
-        time_of_class='10:00'
+        place_of_class=paris_10_chaudron
+        cardio_class=cardio_pump
+    elif date_value=='Saturday':
+        day_of_class=day_dimanche
+        time_of_class='10:15'
+        place_of_class=paris_10_chaudron
+        cardio_class=cardio_famille
     else:
         day_of_class=0
         time_of_class='00:00'
+        cardio_class=0
+        place_of_class=0
 
 
     # These are parameters used to book a class
@@ -74,7 +100,7 @@ def lambda_handler(event, context):
     # Thu 4/27 is id 337855
 
 
-    if ( date_value=='Monday' or date_value=='Wednesday' or date_value=='Friday' ):    
+    if ( date_value=='Monday' or date_value=='Wednesday' or date_value=='Saturday' ):    
         #log in, find id, reserve, log out 
         with requests.Session() as s:
             #login	
@@ -84,10 +110,10 @@ def lambda_handler(event, context):
     
             #find id of class
             print '[FINDID]---------------------------------------------------------------------------'
-            searchload = {'class_search_locationid':paris_10_chaudron,
+            searchload = {'class_search_locationid':place_of_class,
                                  'class_search_day':day_of_class,
                         'class_user_search_recover':user_search_id,
-                               'class_search_level':cardio_pump} 
+                               'class_search_level':cardio_class} 
             p = s.post(search_course_link, data=searchload) 
             #print p.text 
             with open("/tmp/Output.txt", "w") as text_file:
@@ -101,11 +127,19 @@ def lambda_handler(event, context):
                         html_course_id=class_html[1].split('"') 
                         course_id=html_course_id[0]
             link_reserve=base_link_reserve+course_id
+            link_reserve=link_reserve.replace("&amp;","&")
+
             print '\nUse link below to reserve\n<br>'
             print link_reserve
             print '<br>\nUse link above to reserve\n<br>'
-            #text_reserve=p.text
+            text_reserve=p.text
+            text_reserve=''
+
     
+            #response = client.publish(
+            #TopicArn=topic_arn_env,
+            #Message='1\n'+link_reserve+'\n'+text_reserve
+            #)
                           
 	        #Use link to go to book class page
             print '[2]---------------------------------------------------------------------------'
@@ -127,6 +161,12 @@ def lambda_handler(event, context):
             print '<br>\nUse link above to reserve\n<br>'
             text_reserve=r.text
             
+            #Send post confirmation page
+            #response = client.publish(
+            #TopicArn=topic_arn_env,
+            #Message='2\n'+link_reserve+'\n'+text_reserve
+            #)
+            
             #Fetch confirmation results
             date_value=''
             time_value=''
@@ -141,13 +181,24 @@ def lambda_handler(event, context):
                         time_value=line_56_after[0]
 
             response = client.publish(
-            TopicArn='arn:aws:sns:us-east-1:9999999999:GymSuedoise-Notifier',
-            Message='Reservation for:\n'+date_value+'\n'+time_value
+            TopicArn=topic_arn_env,
+            Message='Reservation done for:\n'+date_value+'\n'+time_value
             )
 
 
-            #logout
+	        #Reserve a place
             print '[3]---------------------------------------------------------------------------'	
+            #r = s.get(link_reserve) 
+            #print r.text   
+            #text_reserve=r.text
+    
+            #response = client.publish(
+            #TopicArn=topic_arn_env,
+            #Message='3\n'+link_reserve+'\n'+text_reserve
+            #)
+
+            #logout
+            print '[4]---------------------------------------------------------------------------'	
             r = s.get(logout_link) 
             #print r.text
     else:
